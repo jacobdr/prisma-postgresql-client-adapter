112494954ac038cef20327669b8d8bf1f6d91e2c
Jacob Roberts <jacobdr@gmail.com>
2024-04-14 23:48:30 -0700

    Fix package.json exports

---
+++ prettierrc.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: "always",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 4,
+  trailingComma: "all",
+  /**
+   * @see https://github.com/trivago/prettier-plugin-sort-imports
+   */
+  //  importOrder: [
+  //     "<THIRD_PARTY_MODULES>",
+  //     "^@/tests(.*)$",
+  //     "^@/common(.*)$",
+  //     "^@/trpc(.*)$",
+  //     "^@/server(.*)$",
+  //     "^@/state(.*)$",
+  //     "^@/ui(.*)$",
+  //     "^[./]",
+  // ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
+
+export default prettierConfig;
